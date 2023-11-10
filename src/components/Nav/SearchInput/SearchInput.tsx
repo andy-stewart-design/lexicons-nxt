4f24a410e6ae8505c127938c29fe56007a616891
andy-stewart-design <47165383+andy-stewart-design@users.noreply.github.com>
2023-11-10 07:48:23 -0500

    adding helper text to search buttons

--- src/components/Nav/SearchInput/SearchInput.tsx
+++ src/components/Nav/SearchInput/SearchInput.tsx
@@ -1,6 +1,7 @@
 import { useTextInput } from '@hooks/use-input';
 import { Search, CloseCircled } from '@icons/20';
 import classes from './component.module.css';
+import VisuallyHidden from '@/components/VisuallyHidden';
 
 export default function SearchInput() {
   const [value, setValue] = useTextInput('');
@@ -9,10 +10,12 @@ export default function SearchInput() {
     <div className={`${classes['input']} input-text`}>
       <button className={classes['search']} disabled={!value}>
         <Search />
+        <VisuallyHidden>Search</VisuallyHidden>
       </button>
       <input type="text" value={value} onChange={setValue} />
       <button className={classes['close']} disabled={!value} onClick={() => setValue('')}>
         <CloseCircled />
+        <VisuallyHidden>Clear text</VisuallyHidden>
       </button>
     </div>
   );
