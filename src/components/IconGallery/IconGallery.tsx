f1d9fbc9edb3d0bb30fab7d06f6e395ce675dcdc
andy-stewart-design <47165383+andy-stewart-design@users.noreply.github.com>
2023-11-27 11:54:04 -0500

    Added loading spinner to icon gallery

--- src/components/IconGallery/IconGallery.tsx
+++ src/components/IconGallery/IconGallery.tsx
@@ -3,8 +3,6 @@ import IconCard from './IconCard';
 import classes from './component.module.css';
 
 export default async function IconGallery() {
-  await new Promise((resolve) => setTimeout(resolve, 10000));
-
   const icons = await prisma.icon.findMany({
     include: {
       tags: true,
@@ -31,5 +29,17 @@ export default async function IconGallery() {
 }
 
 export function Loading() {
-  return <div className={classes['gallery']}>Loading...</div>;
+  return (
+    <div className={`${classes['gallery']} ${classes['loading']}`}>
+      <svg width="48" height="48" viewBox="0 0 48 48" fill="none">
+        <path
+          d="M44 24C44 35.0457 35.0457 44 24 44C12.9543 44 4 35.0457 4 24C4 12.9543 12.9543 4 24 4C35.0457 4 44 12.9543 44 24Z"
+          stroke="var(--accent-color)"
+          strokeOpacity="0.2"
+          strokeWidth="6"
+        />
+        <path d="M44 24C44 12.9543 35.0457 4 24 4" stroke="var(--accent-color)" strokeWidth="6" />
+      </svg>
+    </div>
+  );
 }
