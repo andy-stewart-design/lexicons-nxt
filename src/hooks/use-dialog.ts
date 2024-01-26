66d88255bb99c431d2a75e1cdcb7dbfb9077db9c
andy-stewart-design <47165383+andy-stewart-design@users.noreply.github.com>
2024-01-26 06:07:38 -0500

    fixing build error

--- src/hooks/use-dialog.ts
+++ src/hooks/use-dialog.ts
@@ -1,27 +1,27 @@
-import { useState, useCallback, useRef } from 'react';
-import { ModalState } from '@/components/Dialog';
+// import { useState, useCallback, useRef } from 'react';
+// import { ModalState } from '@/components/Dialog';
 
-export type UseDialogReturn = [ModalState, () => void];
-export type DialogSetupHook = (initialState?: ModalState) => UseDialogReturn;
+// export type UseDialogReturn = [ModalState, () => void];
+// export type DialogSetupHook = (initialState?: ModalState) => UseDialogReturn;
 
-export const useDialog: DialogSetupHook = (initialState = 'closed') => {
-  const [modalState, setModalState] = useState<ModalState>(initialState);
-  const showModalTimer = useRef<NodeJS.Timeout | null>(null);
+// export const useDialog: DialogSetupHook = (initialState = 'closed') => {
+//   const [modalState, setModalState] = useState<ModalState>(initialState);
+//   const showModalTimer = useRef<NodeJS.Timeout | null>(null);
 
-  const handleSetModalState = useCallback(
-    function () {
-      if (modalState === 'closed' || modalState === 'closing') {
-        setModalState('opening');
-        if (showModalTimer.current) clearTimeout(showModalTimer.current);
-        showModalTimer.current = setTimeout(() => setModalState('open'), 300);
-      } else if (modalState === 'open' || modalState === 'opening') {
-        setModalState('closing');
-        if (showModalTimer.current) clearTimeout(showModalTimer.current);
-        showModalTimer.current = setTimeout(() => setModalState('closed'), 300);
-      }
-    },
-    [modalState]
-  );
+//   const handleSetModalState = useCallback(
+//     function () {
+//       if (modalState === 'closed' || modalState === 'closing') {
+//         setModalState('opening');
+//         if (showModalTimer.current) clearTimeout(showModalTimer.current);
+//         showModalTimer.current = setTimeout(() => setModalState('open'), 300);
+//       } else if (modalState === 'open' || modalState === 'opening') {
+//         setModalState('closing');
+//         if (showModalTimer.current) clearTimeout(showModalTimer.current);
+//         showModalTimer.current = setTimeout(() => setModalState('closed'), 300);
+//       }
+//     },
+//     [modalState]
+//   );
 
-  return [modalState, handleSetModalState];
-};
+//   return [modalState, handleSetModalState];
+// };
